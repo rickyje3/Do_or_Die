0146e0875e0a5fd854af40fbf4deed70c4b3ec2c
Gavin Cholette <149112460+gavinc711@users.noreply.github.com>
2024-03-25 13:32:28 -0400

    New version
    Redid basically everything

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,46 +11,40 @@ public class PlayerMovement : MonoBehaviour
 	public float speed = 7f;
 	public float turnSmooth = 0.1f;
 	float turnVelocity;
-	public float jump = 5f;
-	public bool grounded;
-	
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-		grounded = true;
-    }
+	public bool grounded = true;
 
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
     // Update is called once per frame
     void Update()
     {
-       float horizontal = Input.GetAxisRaw("Horizontal"); 
-	   float vertical = Input.GetAxisRaw("Vertical"); 
-	   Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-	   
-	   if(direction.magnitude >= 0.1f)
-	   {
-		   float orient = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-		   float turn = Mathf.SmoothDampAngle(transform.eulerAngles.y, orient, ref turnVelocity, turnSmooth);
-		   transform.rotation = Quaternion.Euler(0f, turn, 0f);
-		   
-		   Vector3 move = Quaternion.Euler(0f, orient, 0f) * Vector3.forward;
-		   controller.Move(move.normalized * speed * Time.deltaTime);
-	   }
+        float horizontal = Input.GetAxisRaw("Horizontal");
+		float vertical = Input.GetAxisRaw("Vertical");
+		Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+		
+		if (direction.magnitude >= 0.1f)
+		{
+			float orient = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg +  cam.eulerAngles.y;
+			float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, orient, ref turnVelocity, turnSmooth);
+			transform.rotation = Quaternion.Euler(0f, angle, 0f);
+			
+			Vector3 move = Quaternion.Euler(0f, orient, 0f) * Vector3.forward;
+			controller.Move(move.normalized * speed * Time.deltaTime);
+		}
+		
+		if(Input.GetButtonDown("Jump") && grounded)
+		{
+			rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+			grounded = false;
+		}
     }
-	
-	void FixedUpdate()
-	{
-		if (Input.GetButtonDown("Jump") && grounded)
-	   {
-		   rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
-		   grounded = false;
-	   }
-	}
 	
 	private void OnCollisionEnter(Collision collision)
 	{
-		if(collision.gameObject.CompareTag("Ground"))
+		if(collision.gameObject.tag == "Ground")
 			grounded = true;
 	}
 }
